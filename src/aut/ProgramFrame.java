1c58190a4545c8fbdfa71c1f7026cf8a09373840
AHR96 <ahr96@aut.ac.ir>
2018-04-26 19:02:02 +0430

    creating a frame

--- src/aut/ProgramFrame.java
+++ src/aut/ProgramFrame.java
@@ -1,4 +1,21 @@
 package aut;
 
-public class ProgramFrame {
+import javax.swing.*;
+
+public class ProgramFrame
+{
+    private JFrame mainFrame ;
+
+    public ProgramFrame ()
+    {
+        mainFrame = new JFrame("JDM");
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setPreferredSize(null);
+        
+    }
+
+    public void showFrame ()
+    {
+        mainFrame.setVisible(true);
+    }
 }
