929d56a6203b58bc92bdc1fb4116865fd8a4095a
AHR96 <ahr96@aut.ac.ir>
2018-05-02 15:15:38 +0430

    add listener to menu items

--- src/aut/MenubarPanel.java
+++ src/aut/MenubarPanel.java
@@ -34,26 +34,32 @@ public class MenubarPanel
         pause = new JMenuItem("Pause");
         KeyStroke controlP = KeyStroke.getKeyStroke("control P");
         pause.setAccelerator(controlP);
+        pause.addActionListener(handleActions);
 
         resume = new JMenuItem("Resume");
         KeyStroke controlR = KeyStroke.getKeyStroke("control R");
         resume.setAccelerator(controlR);
+        resume.addActionListener(handleActions);
 
         cancel = new JMenuItem("Cancel");
         KeyStroke controlC = KeyStroke.getKeyStroke("control C");
         cancel.setAccelerator(controlC);
+        cancel.addActionListener(handleActions);
 
         remove = new JMenuItem("Remove");
         KeyStroke controlD = KeyStroke.getKeyStroke("control D");
         remove.setAccelerator(controlD);
+        remove.addActionListener(handleActions);
 
         setting = new JMenuItem("Setting");
         KeyStroke controlS = KeyStroke.getKeyStroke("control S");
         setting.setAccelerator(controlS);
+        setting.addActionListener(handleActions);
 
         exit = new JMenuItem("Exit");
         KeyStroke controlE = KeyStroke.getKeyStroke("control E");
         exit.setAccelerator(controlE);
+        exit.addActionListener(handleActions);
 
         download.add(newDownLoad);
         download.add(resume);
@@ -86,6 +92,30 @@ public class MenubarPanel
             {
                 NewDownloadFrame newDownload = new NewDownloadFrame();
                 newDownload.showFrame();
+            }
+            if(e.getSource().equals(pause))
+            {
+
+            }
+            if(e.getSource().equals(resume))
+            {
+
+            }
+            if(e.getSource().equals(remove))
+            {
+
+            }
+            if(e.getSource().equals(cancel))
+            {
+
+            }
+            if(e.getSource().equals(setting))
+            {
+
+            }
+            if(e.getSource().equals(exit))
+            {
+
             }
         }
     }
